7864f3f3b313f74230eec7d93c6d887f225c85ba
chiarua <2721783259@qq.com>
2023-07-18 16:59:56 +0800

    现在开始我这个相当于正式代码，laofan那个是测试代码

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
-# Suppressing warnings raised by altair in the background
-# (iteration-related deprecation warnings)
-import warnings
-
-warnings.filterwarnings("ignore")
-# Display utilities
 from pprint import pprint
 from IPython.display import display, Markdown
 import draco as drc
 import pandas as pd
 from vega_datasets import data as vega_data
 import altair as alt
+from draco.renderer import AltairRenderer
+import warnings
+
+warnings.filterwarnings("ignore")
+# Suppressing warnings raised by altair in the background
+# (iteration-related deprecation warnings)
+
 
 # Loading data to be explored
 df: pd.DataFrame = vega_data.seattle_weather()
 df.head()
 
-#df=pd.read_csv('cars.csv')
-
-
-
-
-#print(df)
-
 data_schema = drc.schema_from_dataframe(df)
-#pprint(data_schema)
 data_schema_facts = drc.dict_to_facts(data_schema)
-#pprint(data_schema_facts)
-from draco.renderer import AltairRenderer
 
 input_spec_base = data_schema_facts + [
     "entity(view,root,v0).",
     "entity(mark,v0,m0)."
 ]
-#pprint(input_spec_base)
+# pprint(input_spec_base)
 d = drc.Draco()
 renderer = AltairRenderer()
 
 
-
 def recommend_charts(
-    spec: list[str], draco: drc.Draco, num: int = 5, labeler=lambda i: f"CHART {i+1}"
+        spec: list[str], draco: drc.Draco, num: int = 5, labeler=lambda i: f"CHART {i + 1}"
 ) -> dict[str, dict]:
     # Dictionary to store the generated recommendations, keyed by chart name
     chart_specs = {}
@@ -53,26 +43,29 @@ def recommend_charts(
         chart = renderer.render(spec=spec, data=df)
         # Adjust column-faceted chart size
         if (
-            isinstance(chart, alt.FacetChart)
-            and chart.facet.column is not alt.Undefined
+                isinstance(chart, alt.FacetChart)
+                and chart.facet.column is not alt.Undefined
         ):
-            #print('nihao'*10)
+            # print('nihao'*10)
             chart = chart.configure_view(continuousWidth=130, continuousHeight=130)
         display(chart)
-        chart.save('filename'+str(i)+'.html')
+        chart.save('filename' + str(i) + '.html')
 
     return chart_specs
-input_spec=input_spec_base
-#recommend_charts(spec=input_spec, draco=d)
 
+
+input_spec = input_spec_base
+
+
+# recommend_charts(spec=input_spec, draco=d)
 
 
 def rec_from_generated_spec(
-    marks: list[str],
-    fields: list[str],
-    encoding_channels: list[str],
-    draco: drc.Draco,
-    num: int = 1,
+        marks: list[str],
+        fields: list[str],
+        encoding_channels: list[str],
+        draco: drc.Draco,
+        num: int = 1,
 ) -> dict[str, dict]:
     input_specs = [
         (
@@ -99,6 +92,7 @@ def rec_from_generated_spec(
         recs = recs | recommend_charts(spec=spec, draco=draco, num=num, labeler=labeler)
 
     return recs
+
 
 recommendations = rec_from_generated_spec(
     marks=["point", "bar", "line", "rect"],
--- wlytesting.py
+++ wlytesting.py
@@ -1,41 +1,21 @@
-# Suppressing warnings raised by altair in the background
-# (iteration-related deprecation warnings)
-import warnings
-
-warnings.filterwarnings("ignore")
-
-# Display utilities
-from pprint import pprint
 from IPython.display import display, Markdown
-
 import draco as drc
 import pandas as pd
 from vega_datasets import data as vega_data
 import altair as alt
-
-# Loading data to be explored
-df: pd.DataFrame = vega_data.seattle_weather()
-df.head()
-
-data_schema = drc.schema_from_dataframe(df)
-pprint(data_schema)
-
-data_schema_facts = drc.dict_to_facts(data_schema)
-pprint(data_schema_facts)
-
 from draco.renderer import AltairRenderer
+import warnings
 
-input_spec_base = data_schema_facts + [
-    "entity(view,root,v0).",
-    "entity(mark,v0,m0).",
-]
-d = drc.Draco()
-renderer = AltairRenderer()
+warnings.filterwarnings("ignore")
 
 
+# Suppressing warnings raised by altair in the background
+# (iteration-related deprecation warnings)
+
+# 原有推荐函数
 def recommend_charts(
-    spec: list[str], draco: drc.Draco, num: int = 5, labeler=lambda i: f"CHART {i+1}"
-) -> dict[str, dict]:
+        spec: list[str], draco: drc.Draco, num: int = 5, labeler=lambda i: f"CHART {i + 1}"
+):
     # Dictionary to store the generated recommendations, keyed by chart name
     chart_specs = {}
     for i, model in enumerate(draco.complete_spec(spec, num)):
@@ -48,13 +28,58 @@ def recommend_charts(
         chart = renderer.render(spec=spec, data=df)
         # Adjust column-faceted chart size
         if (
-            isinstance(chart, alt.FacetChart)
-            and chart.facet.column is not alt.Undefined
+                isinstance(chart, alt.FacetChart)
+                and chart.facet.column is not alt.Undefined
         ):
             chart = chart.configure_view(continuousWidth=130, continuousHeight=130)
         display(chart)
+        print(output_path + 'filename' + str(i) + '.html')
+        chart.save(output_path + 'filename' + str(i) + '.html')
 
-    return chart_specs
+    # return chart_specs(-> dict[str, dict])
 
-input_spec = input_spec_base
-recommend_charts(spec=input_spec, draco=d);
+
+def get_csvfile(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+
+def get_output_address():
+    address = input("Please input the address where you want the picture to be output: ")
+    return address
+
+
+# 基础事实集的创建
+def generate_spec_base(df):
+    data_schema = drc.schema_from_dataframe(df)
+    data_schema_facts = drc.dict_to_facts(data_schema)
+    input_spec_base = data_schema_facts + [
+        "entity(view,root,v0).",
+        "entity(mark,v0,m0)."
+    ]
+    return input_spec_base
+
+
+# 被generate_spec_base()函数代替
+'''
+df: pd.DataFrame = get_csvfile("")
+df.head()
+
+data_schema = drc.schema_from_dataframe(df)
+data_schema_facts = drc.dict_to_facts(data_schema)
+input_spec_base = data_schema_facts + [
+    "entity(view,root,v0).",
+    "entity(mark,v0,m0)."
+]
+'''
+
+path = input("Please input the path of the csv file: ")
+output_path = get_output_address() + '\\'
+df = get_csvfile(path)
+d = drc.Draco()
+renderer = AltairRenderer()
+input_spec_base = generate_spec_base(df)
+recommend_charts(spec=input_spec_base, draco=d)
+# pprint(input_spec_base)
+# C:\Users\27217\Documents\GitHub\testing-7-16-23\laofan\cars.csv
+# C:\Users\27217\Desktop\testing generate charts
