ca424bd9d5de2887f4ba37fa110b94a030216e35
李青阳 <3131492575@qq.com>
2023-07-23 12:35:56 +0800

    更新显示逻辑

--- framework/framework.py
+++ framework/framework.py
@@ -13,46 +13,40 @@ st.title("可视化推荐")
 st.divider()
 
 with st.sidebar:
-    output_address = st.text_input('输出地址',placeholder='add your output address here')
-    submit_button1 = st.button('确定',key='button1')
-    if output_address == '':
-        st.info('Try to put your output_address into the box.')
+    input_NL = st.text_input('您的需求', placeholder='add your requirements here')
+    submit_button1 = st.button('提交',key='button1')
 
-    input_NL = st.text_input('您的需求',placeholder='add your requirements here')
-    submit_button2 = st.button('提交', key='button2')
+    if input_NL == '' and submit_button1:
+        st.warning('make sure you have put your requirements in the box.')
+        st.stop()
+
     if input_NL == '':
-        st.info('Try to type some requirements.')
+        st.stop()
 
     input_file = st.file_uploader('文件上传',type=['csv'])
-
     if input_file is None:
-        st.info('Please upload your data.')
         st.stop()
-    else:
-        num = st.slider('生成数量', 0, 20, 5)
-        if st.checkbox('显示数据'):
-            st.subheader('数据')
-            df = load_data(input_file)
-            st.write(df)
 
-    submit_button3 = st.button('开始生成', key='button3')
+    num = st.slider('生成数量', 0, 20, 5)
+    if st.checkbox('显示数据'):
+        st.subheader('数据')
+        df = load_data(input_file)
+        st.write(df)
 
-if submit_button3:
+    submit_button2 = st.button('开始生成', key='button2')
+
+if submit_button2:
     with st.spinner('Wait for it...'):
         time.sleep(3)
 
     flag = 0
-    for i in range(10, 16, 1):
+    for i in range(11, 17, 1):
         flag += 1
         if flag > num:
             break
+
         with open(f'.\\rec_ch{i}.html', "r") as f:
             h = f.read()
             components.html(h,height=400)
-
-
-
-
-
 
 
